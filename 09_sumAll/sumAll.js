ed82d846cbcb36feb87d1e40adf80e202f425ca2
yosvaldo <219594820+yosvaldo@users.noreply.github.com>
2025-08-27 21:09:04 +0700

    initial commit for 08 and 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let result = 0;
 
+    if (!(start < 0 || end < 0 || start %1 != 0 || end %1 != 0 || typeof start != 'number' || typeof end != 'number')){
+
+        if (start<=end){
+        for (let i=start; i<=end; i++){
+            result += i;
+        }
+        }
+
+        else {
+        for (let i=end; i<=start; i++){
+            result += i;
+        }    
+        }
+    }
+
+    else {
+        result = 'ERROR';
+    }
+
+    return result;
 };
 
 // Do not edit below this line
