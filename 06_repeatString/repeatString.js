315e3ea66f3d6ba5f9cd81bdb2aa472617d25ee0
yosvaldo <219594820+yosvaldo@users.noreply.github.com>
2025-08-27 18:04:02 +0700

    initial commit

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let result = "";
+  
+  if (num<0){
+    result = 'ERROR';
+  }
 
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
+  return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+let length = text.length;
+let string = '';
+for (let i=length-1; i>=0; i--){
+    string += text.charAt(i);
+}
+return string;
 };
 
 // Do not edit below this line
