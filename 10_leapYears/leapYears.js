5074b762ea7200ca9ea5a75df937ab661a858f16
yosvaldo <219594820+yosvaldo@users.noreply.github.com>
2025-08-28 11:30:30 +0700

    initial commit for 10 & 11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+    let result='';
 
+    if(year % 4 === 0 && (year % 100 != 0 || year % 400 === 0)){
+        result = true;
+        }
+    else{
+        result = false;
+    }
+    
+    return result;
+    }
+    
 // Do not edit below this line
 module.exports = leapYears;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let cel;
+  cel = (fahrenheit - 32) * 5 / 9;
+  var celcius = Math.round(cel * 10) / 10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fah;
+  fah = celcius * 9 / 5 + 32;
+  var fahrenheit = Math.round(fah * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
